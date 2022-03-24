03ae1a3e43a12c8028c263265cbc302a6b6c4d98
wilsonchen0305 <99401840+wilsonchen0305@users.noreply.github.com>
2022-03-24 20:09:29 +0800

    App test change

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react with James Hush! link', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react with James Hush!/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
